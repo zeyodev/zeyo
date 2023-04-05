c874ebc61efb04ac98e4b137baba9cc85a0ddb03
evandrouzeda <evandrouzeda@gmail.com>
2023-04-05 12:06:27 +0000

    exportando com type parameter

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import Zeyo from "./zeyo";
 type ZeyoType = Zeyo<keyof HTMLElementTagNameMap>
-export { ZeyoType as Zeyo }
+export { ZeyoType as Zeyo, Zeyo as ZeyoAs }
 
 export default function Z<T extends keyof HTMLElementTagNameMap>(tagName: T): Zeyo<T> {
     return new Zeyo(tagName)
--- src/properties/children.ts
+++ src/properties/children.ts
@@ -3,7 +3,7 @@ import Zeyo from "../zeyo";
 
 export default function Children<Base extends ZeyoConstructor>(base: Base) {
     return class extends base {
-        children<T extends keyof HTMLElementTagNameMap>(...child: Array<Zeyo<T> | string>){
+        children(...child: Array<Zeyo<keyof HTMLElementTagNameMap> | string>){
             child.forEach(c => {
                 if (typeof c === "string")
                     this.element.innerHTML += c
