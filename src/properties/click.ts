f16f389eb08d5baafbb5f942714c74a7e452ccad
evandrouzeda <evandrouzeda@gmail.com>
2023-10-16 18:36:09 +0000

    criando clickevent

--- src/properties/click.ts
+++ src/properties/click.ts
@@ -2,13 +2,18 @@ import { ZeyoConstructor } from "../../index"
 
 export default function Click<T, Base extends ZeyoConstructor<T>>(base: Base) {
     return class extends base {
-        click(cb: (e: MouseEvent) => void) {
-            (this.element as any).onclick = cb
+        click(cb: (object: this, event: MouseEvent) => void) {
+            (this.element as any).onclick = (event: MouseEvent) => cb(this, event)
             return this
         }
 
         clickthis<T extends keyof HTMLElementTagNameMap>(cb: (e: HTMLElementTagNameMap[T]) => void) {
             (this.element as any).onclick = () => cb((this.element as any))
+            return this
+        }
+        
+        clickevent(cb: (e: MouseEvent) => void) {
+            (this.element as any).onclick = cb
             return this
         }
     }
