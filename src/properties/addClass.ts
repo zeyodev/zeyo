ea963b0bf61956d7057ecfc803776b17297ea497
evandrouzeda <evandrouzeda@gmail.com>
2023-11-04 14:22:41 +0000

    filtrando tokens vazios

--- src/properties/addClass.ts
+++ src/properties/addClass.ts
@@ -3,7 +3,7 @@ import { ZeyoConstructor } from "../../index"
 export default function AddClass<T, Base extends ZeyoConstructor<T>>(base: Base) {
     return class extends base {
         class(...tokens: string[]) {
-            (this.element as any).classList.add(...tokens)
+            (this.element as any).classList.add(...(tokens.filter(t => t !== "")))
             return this
         }
     }
