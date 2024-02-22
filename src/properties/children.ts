afd734a2220fdb8803946a03e3c9633f95882d7a
evandrouzeda <evandrouzeda@gmail.com>
2024-02-22 19:16:21 +0000

    adicionando childlist para guardar os zFilhos do zElemento

--- src/properties/children.ts
+++ src/properties/children.ts
@@ -3,7 +3,9 @@ import Zeyo from "../zeyo";
 
 export default function Children<T, Base extends ZeyoConstructor<T>>(base: Base) {
     return class extends base {
+        childList: Array<Zeyo<keyof HTMLElementTagNameMap> | string> = []
         children(...child: Array<Zeyo<keyof HTMLElementTagNameMap> | string>){
+            this.childList = child
             child.forEach(c => {
                 if (typeof c === "string")
                     (this.element as any).innerHTML += c
