f172b5f2b1d360e0bfdb49ad300dd476100af1b2
Evandro Uzeda <49155191+evandrouzeda@users.noreply.github.com>
2025-03-11 21:40:47 -0300

    createElement

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,17 @@ export { ZeyoType as Zeyo, Zeyo as ZeyoAs }
 export default function Z<T extends keyof HTMLElementTagNameMap>(tagName: T): Zeyo<T> {
     return new Zeyo(tagName)
 }
+
+function createElement<K extends keyof HTMLElementTagNameMap>(tag: K) {
+    return (...children: Array<Zeyo<keyof HTMLElementTagNameMap> | string>): Zeyo<K> => {
+        return new Zeyo(tag).children(...children);
+    };
+}
+
+// Example exports for common tags
+export const div = createElement("div");
+export const a = createElement("a");
+export const p = createElement("p");
+export const span = createElement("span");
+export const input = createElement("input");
+export const button = createElement("button");
